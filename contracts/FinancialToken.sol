6d2b0c172122a299a4e5bedf2433b5ecb673ae4f
masaun <masanoriuno@gmail.com>
2019-04-13 08:31:03 +0200

    Inherit ERC20Mintable and ERC20Burnable

--- contracts/FinancialToken.sol
+++ contracts/FinancialToken.sol
@@ -4,11 +4,12 @@ import "zos-lib/contracts/Initializable.sol";
 import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/ERC20Detailed.sol";
-// import "openzeppelin-eth/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity-2.1.1/contracts/token/ERC20/ERC20Burnable.sol";
 // import "openzeppelin-eth/contracts/token/ERC20/ERC20Detailed.sol";
 
 
-contract FinancialToken is Initializable, ERC20, ERC20Detailed {
+contract FinancialToken is Initializable, ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
     
     string _name = "FinancialToken";
     string _symbol = "FNL";
@@ -32,23 +33,7 @@ contract FinancialToken is Initializable, ERC20, ERC20Detailed {
     // } 
 
 
-    // Fail
-    // function initialize(
-    //     string memory name,
-    //     string memory symbol,
-    //     uint8 decimals
-    // ) 
-    //     public 
-    //     ERC20Detailed(_name, _symbol, _decimals)
-    //     initializer
-    // {
-    //     _mint(msg.sender, INITIAL_SUPPLY);
-    // }
 
 
-    // Fail
-    // function initialize() public initializer ERC20Detailed("FinancialToken", "FNL", 18) {
-    //     _mint(msg.sender, INITIAL_SUPPLY);
-    // }
 
 }
