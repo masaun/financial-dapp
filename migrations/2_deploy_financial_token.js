f9fb22c1703e2c2bf03f0273827a89e15ae84175
masaun <masanoriuno@gmail.com>
2019-04-15 10:01:42 +0200

    Success to get FinancialToken's name by solving Promise pending

--- migrations/2_deploy_financial_token.js
+++ migrations/2_deploy_financial_token.js
@@ -5,7 +5,7 @@ module.exports = function(deployer, network, accounts) {
     const symbol = "FNL";
     const decimals = 18;
     //const initSupply = 10000e18;
-    const initSupply = web3.utils.toBN(100*(10**decimals));
+    const initSupply = web3.utils.toBN(100*(10**decimals));  // 100 ether
     
     return deployer.then(()=>{
         return deployer.deploy(
--- test/1_test_financial_token.js
+++ test/1_test_financial_token.js
@@ -30,12 +30,18 @@ contract("FinancialToken", accounts => {
 
     it("Get name of financialTokenInstance", async () => {
         // Get instance of FinancialToken contract
-        const hoge = FinancialToken.deployed()
-                      .then((i) => {
-                          instance = i;
-                          instance_name = instance.name();
-                          console.log('=== instance_name ===', instance_name);
-                      });
+        // const hoge = FinancialToken.deployed()
+        //               .then((i) => {
+        //                   instance = i;
+        //                   instance_name = instance.name();
+        //                   console.log('=== instance_name ===', instance_name);
+        //               });
+
+        const instance = await FinancialToken.deployed()
+        //console.log('=== name of financialToken ===', instance.name())
+        instance.name().then((result) => {
+          console.log('=== name of financialToken ===', result);  // Success（"FinancialToken"）
+        });
     });
 
 
@@ -49,10 +55,32 @@ contract("FinancialToken", accounts => {
         const _to = '0x5Eb9CdAE61c07ADD7bf703Eb12eDFD1cecc22A41';
         const _value = 10;
 
-        const financial_token = await new web3.eth.Contract(FinancialToken.abi, FinancialToken.address);
+        let financial_token = await new web3.eth.Contract(FinancialToken.abi, FinancialToken.address);
         //console.log('=== financial_token ===', financial_token);
 
-        const response_mintToken = financial_token.methods.mintToken(_to, _value).send({from: accounts[0]});
+        let response_mintToken = financial_token.methods.mintToken(_to, _value).send({from: accounts[0]});
         console.log('=== mintToken function ===', response_mintToken);
+
+
+        response_mintToken.then((result) => {
+          console.log('==== mintToken function v2 ===', result);
+        });
+
     });
+
+
+
+    // it("Mint token", async () => {
+    //     const accounts = await web3.eth.getAccounts();
+    //     console.log('=== accounts ===', accounts);  // Success
+
+    //     const _to = '0x5Eb9CdAE61c07ADD7bf703Eb12eDFD1cecc22A41';
+    //     const _value = 10;
+
+    //     const financial_token = await new web3.eth.Contract(FinancialToken.abi, FinancialToken.address);
+    //     //console.log('=== financial_token ===', financial_token);
+
+    //     const response_mintToken = financial_token.methods.mintToken(_to, _value).send({from: accounts[0]});
+    //     console.log('=== mintToken function ===', response_mintToken);
+    // });
 });
